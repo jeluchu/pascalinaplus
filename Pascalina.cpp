50cfe4bf1853fa2beec6303ccb870cf7ed4ffe52
Jeluchu <jesusmarcalderon@gmail.com>
2017-11-14 10:24:03 +0100

    Pascalina v0.5

---
+++ Pascalina.cpp
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+
+    int op1;
+    int op2;
+    int Opcion;
+    int Resultado;
+    int n;
+
+    printf("PASCALINA v0.5\n\n");
+
+    printf("Bienvenido/a a 'Pascalina', la calculadora adivina, en ella podrás realizar diversas operaciónes con dos números\n");
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+    printf("Introducir el primer número deseado: ");
+    scanf(" %i", &op1);
+
+    printf("Introducir el segundo número deseado: ");
+    scanf(" %i", &op2);
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+    fflush(stdin);
+
+    printf("\t¿Qué operación deseas realizar?\n");
+    printf("\tPulsa 1 si deseas sumar\n");
+    printf("\tPulsa 2 si deseas restar\n");
+    printf("\tPulsa 3 si deseas multiplicar\n");
+    printf("\tPulsa 4 si deseas dividir\n");
+    printf("\tPulsa 5 si deseas hacer una Raíz Cuadrada\n");
+    printf("\tPulsa 6 si deseas cambiar los valores\n");
+    printf("\tPulsa 0 si deseas salir\n\n");
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("Escoge la opción deseada: ");
+
+
+do{
+    scanf("%i", &Opcion);
+
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+    switch(Opcion) {
+    case 0:
+           printf("Gracias por usar 'Pascalina', esperamos que te haya sido de utilidad. Muchas gracias\n");
+           return EXIT_SUCCESS;
+    break;
+
+    case 1: Resultado = op1+op2; /* Suma */
+        printf("El resultado de la suma es: ");
+        printf("%i\n\n", Resultado);
+    break;
+
+    case 2: Resultado = op1-op2; /* Resta */
+        printf("El resultado de la resta es: ");
+        printf("%i\n\n", Resultado);
+    break;
+
+    case 3: Resultado = op1*op2; /* Multiplicación */
+        printf("El resultado de la multiplicación es: ");
+        printf("%i\n\n", Resultado);
+    break;
+
+    case 4: Resultado = op1/op2; /* División */
+        printf("El resultado de la división es: ");
+        printf("%i\n\n", Resultado);
+    break;
+
+    case 5: /* Raíz Cuadrada */
+        printf("Introducir el número deseado: ");
+           scanf("%i",&n);
+           n = sqrt(n);
+           printf("El resultado de la raíz cuadrada es: %i\n\n",n);
+    break;
+
+    case 6:
+        printf("Introducir el primer número deseado: ");
+        scanf(" %i", &op1);
+        printf("Introducir el segundo número deseado: ");
+        scanf(" %i", &op2);
+        printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    break;
+
+    default: printf("Por favor seleccione una opción correcta\n");
+
+  }
+
+
+    printf("Para realizar otra operación, marque una opción: ");
+ }while(Opcion!=0);
+ }
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Pascalina
 Pascalina is a basic calculator in which you can do various operations with two numbers
+
+# VERSIONS
+
+## v0.5
+- Fix problems with 'Square Root'
+- Add new options
+- You can exit pressing option '0'
+- You can change the numbers pressing option '6'
+- In this version you can made operations: add, subtraction, multiplicate, division and square root
+
+# INSTRUCTIONS
+- To execute 'Pascalina', will request you two numbers to carry out the operations
+- Then you can choose the option of the desired operation
+- At the end, you will get the result, and you can perform any other operation
